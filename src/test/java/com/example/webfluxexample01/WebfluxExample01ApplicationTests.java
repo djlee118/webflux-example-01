68458e4a4c6292f20aac63443c656a914e0eb858
djlee118 <djlee118@nate.com>
2022-08-13 22:10:01 +0900

    Initial commit

---
+++ src/test/java/com/example/webfluxexample01/WebfluxExample01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxexample01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxExample01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
